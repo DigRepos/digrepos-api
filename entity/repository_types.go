6f47354552f417a52da321e27dc21f741359c283
yuta4j1 <kascado.ys10@gmail.com>
2019-09-08 23:36:50 +0900

    fix: json type

--- entity/repository_types.go
+++ entity/repository_types.go
@@ -1,13 +1,23 @@
 package entity
 
+type Owner struct {
+	Name      string `json:"name"`
+	AvatarUrl string `json:"avatarUrl"`
+}
+
 type RepositorySummary struct {
-	Id          int64  `json:"id"`
-	FullName    string `json:"fullName"`
-	Star        int    `json:"star"`
-	Owner       string `json:"ownerName"`
-	Description string `json:"description"`
-	UpdatedAt   string `json:"updatedAt"`
-	Language    string `json:"language"`
-	Size        int    `json:"size"`
-	License     string `json:"license"`
+	Id            int64    `json:"id"`
+	HTMLURL       string   `json:"url"`
+	FullName      string   `json:"fullName"`
+	Star          int      `json:"star"`
+	ForksCount    int      `json:"forksCount"`
+	WatchersCount int      `json:"watchersCount"`
+	Owner         Owner    `json:"owner"`
+	Description   string   `json:"description"`
+	Homepage      string   `json:"homepage"`
+	Topics        []string `json:"topics"`
+	UpdatedAt     string   `json:"updatedAt"`
+	Language      string   `json:"language"`
+	Size          int      `json:"size"`
+	License       string   `json:"license"`
 }
--- usecase/repository.go
+++ usecase/repository.go
@@ -13,6 +13,10 @@ func Repositories(ctx context.Context, query string) ([]entity.RepositorySummary
 	opts := &github.SearchOptions{
 		Sort:  "stars",
 		Order: "asc",
+		ListOptions: github.ListOptions{
+			Page:    1,
+			PerPage: 100,
+		},
 	}
 
 	repoDatas := []entity.RepositorySummary{}
@@ -25,15 +29,23 @@ func Repositories(ctx context.Context, query string) ([]entity.RepositorySummary
 	for _, repo := range repositories {
 		fmt.Println(*repo.Name)
 		summary := entity.RepositorySummary{
-			Id:          *repo.ID,
-			FullName:    *repo.FullName,
-			Star:        *repo.StargazersCount,
-			Owner:       repo.Owner.GetName(),
-			Description: *repo.Description,
-			UpdatedAt:   repo.UpdatedAt.String(),
-			Language:    *repo.Language,
-			Size:        *repo.Size,
-			License:     repo.License.GetName(),
+			Id:            *repo.ID,
+			HTMLURL: *repo.HTMLURL,
+			FullName:      *repo.FullName,
+			Star:          *repo.StargazersCount,
+			ForksCount:    *repo.ForksCount,
+			WatchersCount: *repo.WatchersCount,
+			Owner:         entity.Owner{
+				Name: repo.Owner.GetName(),
+				AvatarUrl: repo.Owner.GetAvatarURL(),
+			},
+			Description:   *repo.Description,
+			Homepage:      repo.GetHomepage(),
+			Topics:        repo.Topics,
+			UpdatedAt:     repo.UpdatedAt.String(),
+			Language:      *repo.Language,
+			Size:          *repo.Size,
+			License:       repo.License.GetName(),
 		}
 		repoDatas = append(repoDatas, summary)
 	}
