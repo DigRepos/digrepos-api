35572163342ffe1cf9e4df8097baf7d6e42d655a
yuta4j1 <kascado.ys10@gmail.com>
2019-09-23 23:46:14 +0900

    feat: 'filter' endpoint

--- infrastructure/router.go
+++ infrastructure/router.go
@@ -2,8 +2,11 @@ package infrastructure
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"strings"
 
+	entity "../entity"
 	repository "../usecase"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -25,5 +28,31 @@ func Run() {
 		return c.JSON(http.StatusOK, repos)
 	})
 
+	e.GET("/filter", func(c echo.Context) error {
+		fmt.Println("query", c.QueryString())
+		ctx := context.Background()
+		filter := new(entity.Filter)
+		fmt.Println(c.QueryParams())
+		// TODO QueryParamsから構造体へのバインディングをやるレシーバ関数を
+		// Filter構造体に追加する
+		// filter.Keywords = c.QueryParam("keywords")
+		filter.Star = entity.Star{Low: c.QueryParam("star[low]"), High: c.QueryParam("star[high]")}
+		filter.Language = c.QueryParam("language")
+		filter.License = c.QueryParam("license")
+
+		repos, err := repository.Repositories(ctx, filter.BuildQuery())
+		if err != nil {
+			e.Logger.Fatal(err)
+		}
+		return c.JSON(http.StatusOK, repos)
+	})
+
 	e.Logger.Fatal(e.Start(":1234"))
+}
+
+func builtIntoQuery(query string, part string) string {
+	if strings.Trim(part, " ") != "" {
+		query = query + part
+	}
+	return query
 }
